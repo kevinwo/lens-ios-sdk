ef40440aca18ecbaffbf40becdf64c130b78d848
Kevin Wolkober <kevin.wolkober@gmail.com>
2023-04-09 12:21:36 -0400

    Add package base and example project base

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Kevin Wolkober on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
